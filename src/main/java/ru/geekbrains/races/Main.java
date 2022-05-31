03ac5827e01b79c3d37a448b545bde1c1476bc3d
GeekBrains <sas431@mail.ru>
2022-05-31 09:36:41 +0300

    HW5

--- src/main/java/ru/geekbrains/races/Main.java
+++ src/main/java/ru/geekbrains/races/Main.java
@@ -2,68 +2,103 @@ package ru.geekbrains.races;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import static ru.geekbrains.races.Main.HALF_CARS_COUNT;
 
-/**1.
- * Приведённый в методичке код перенести в новый проект.
- Организуем гонки:
- Все участники должны стартовать одновременно, несмотря на то, что на подготовку у каждого из них уходит разное время.
-
- В туннель не может заехать одновременно больше половины участников (условность).
- Попробуйте всё это синхронизировать.
- Только после того как все завершат гонку, нужно выдать объявление об окончании.
- После окончания гонки определить победителя.
- Можете корректировать классы (в т.ч. конструктор машин) и добавлять объекты классов из пакета util.concurrent.*/
+/** Проект создал в одном файле, хотя скорее всего нужно было разнести по разным.*/
 
 public class Main {
 
     public static final int CARS_COUNT = 4;
+    public static final int HALF_CARS_COUNT = CARS_COUNT/2;
 
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
+        CountDownLatch countDownLatch = new CountDownLatch(CARS_COUNT);
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cyclicBarrier, countDownLatch);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        try {
+            cyclicBarrier.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            cyclicBarrier.await();
+            cyclicBarrier.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
 
 class Car implements Runnable {
     private static int CARS_COUNT;
+    private  static boolean winnerFound;
+    private static Lock win = new ReentrantLock();
+
     static {
         CARS_COUNT = 0;
     }
+
     private Race race;
     private int speed;
     private String name;
+    private int count;
+    private final CyclicBarrier cyclicBarrier;
+    private final CountDownLatch countDownLatch;
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+
+    public int getCount() {
+        return count;
+    }
+
+    public Car(Race race, int speed, CyclicBarrier cyclicBarrier, CountDownLatch countDownLatch) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cyclicBarrier = cyclicBarrier;
+        this.countDownLatch = countDownLatch;
     }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cyclicBarrier.await();
+            cyclicBarrier.await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+            checkWinner(this);
+            cyclicBarrier.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
+    }
+
+    public synchronized void checkWinner(Car c) {
+        if (!winnerFound) {
+            System.out.println(c.name + " - WIN");
+            winnerFound = true;
         }
     }
 }
@@ -74,6 +109,7 @@ abstract class Stage {
     public String getDescription() {
         return description;
     }
+
     public abstract void go(Car c);
 }
 
@@ -82,6 +118,7 @@ class Road extends Stage {
         this.length = length;
         this.description = "Дорога " + length + " метров";
     }
+
     @Override
     public void go(Car c) {
         try {
@@ -95,21 +132,26 @@ class Road extends Stage {
 }
 
 class Tunnel extends Stage {
+    Semaphore semaphore = new Semaphore(HALF_CARS_COUNT);
+
     public Tunnel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
     }
+
     @Override
     public void go(Car c) {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                semaphore.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                semaphore.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -119,7 +161,11 @@ class Tunnel extends Stage {
 
 class Race {
     private ArrayList<Stage> stages;
-    public ArrayList<Stage> getStages() { return stages; }
+
+    public ArrayList<Stage> getStages() {
+        return stages;
+    }
+
     public Race(Stage... stages) {
         this.stages = new ArrayList<>(Arrays.asList(stages));
     }
